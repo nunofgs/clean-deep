3b3c264cab19289055fee4eed6936193087514ee
Daniel Skogly <daniel@klungo.no>
2021-12-21 18:48:46 +0100

    Fix whitespace in types

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,14 @@ declare function cleanDeep<T>(object: T, options?: CleanOptions): Partial<T>;
 export default cleanDeep;
 
 export type CleanOptions = {
-	cleanKeys?: string[];
-	cleanKeysPrefixes?: string[];
-	cleanValues?: string[];
-	cleanValuesPrefixes?: string[];
-	emptyArrays?: boolean;
-	emptyObjects?: boolean;
-	emptyStrings?: boolean;
-	NaNValues?: boolean;
-	nullValues?: boolean;
-	undefinedValues?: boolean;
+    cleanKeys?: string[];
+    cleanKeysPrefixes?: string[];
+    cleanValues?: string[];
+    cleanValuesPrefixes?: string[];
+    emptyArrays?: boolean;
+    emptyObjects?: boolean;
+    emptyStrings?: boolean;
+    NaNValues?: boolean;
+    nullValues?: boolean;
+    undefinedValues?: boolean;
 };
